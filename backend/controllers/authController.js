aa5a34858be8f10e470bb2b58a69eed9adb69862
Dev Gupta <devguptamaholi@gmail.com>
2023-10-04 19:11:43 +0530

    Added forget password script

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,9 @@
 const User = require("../models/userModel");
 const Station=require("../models/stationModel")
 const generateToken = require("../config/generateToken");
+const nodemailer = require("nodemailer");
+const bcrypt=require("bcryptjs")
+const crypto = require("crypto");
 
 const registerUser = async (req, res) => {
   try {
@@ -46,6 +49,7 @@ const loginUser = async (req, res) => {
     {
         return res.status(401).json({message:"Not authorized to access"});
     }
+    
     if (user && (await user.matchPassword(password))) {
       res.status(200).json({
         _id: user._id,
@@ -61,4 +65,81 @@ const loginUser = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
-module.exports = { registerUser, loginUser };
+ const otpStorage = new Map();
+const forgetPassword=async(req,res)=>{
+
+
+ const email  = req.body.email;
+ const user = await User.findOne({ email });
+ if (!user) {
+   return res.status(401).json({ message: "Not authorized to access" });
+ }
+ var smtpConfig = {
+   host: "smtp.gmail.com",
+   port: process.env.EMAIL_PORT,
+   secure:true,
+   service:'GMAIL',
+   auth: {
+     user: process.env.USER_NAME,
+     pass: process.env.USER_PASS,
+   },
+ };
+
+ const transporter = nodemailer.createTransport(smtpConfig);
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+   otpStorage.set(email, otp);
+  const mailOptions = {
+    to: email,
+    subject: "Password Reset OTP",
+    text: `Your OTP for password reset is: ${otp}`,
+  };
+
+  await transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      res.status(500).send('Error sending OTP');
+    } else {
+      console.log(otpStorage.get(email))
+      console.log('Email sent:', info.response);
+      res.status(200).send('OTP sent successfully');
+    }
+  });
+};
+
+
+
+const confirmPassword=async(req,res)=>{
+  try{
+     const {email,otp}=req.body;
+    
+     if(otpStorage.get(email)===otp)
+     {  
+       otpStorage.delete(email)
+       return res.status(201).json({open:true,message:"Re-enter new password"});
+     }
+     res.status(403).json({open:false,message:"Provide the correct otp"});
+  }
+  catch(error)
+  {
+    res.status(500).json({error:error.message})
+  }
+
+};
+const updatenewPassword=async(req,res)=>{
+  try {
+    const { email, newPassword } = req.body;
+    const user = await User.findOne({ email });
+  
+
+    user.password = newPassword;
+    user.save();
+    res.status(201).json({message:"Updation successfull"})
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
+
+
+};
+
+module.exports = { registerUser, loginUser,forgetPassword ,confirmPassword,updatenewPassword};
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/authController");
+const { registerUser, loginUser,forgetPassword ,confirmPassword,updatenewPassword} = require("../controllers/authController");
 const router = express.Router();
 router.route("/").post(registerUser);
 router.post("/login", loginUser);
+router.post("/forgetpassword",forgetPassword)
+router.get("/confirmpassword",confirmPassword)
+router.put("/updatepassword",updatenewPassword)
 module.exports = router;
